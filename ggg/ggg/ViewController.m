b13d8222fbc58c3b2f7d524c606cc6cb288ab7e3
Go <go@godemacbook-pro.local>
2015-10-20 09:09:55 +0800

    我要上传谁能当我

---
+++ ggg/ggg/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  ggg
+//
+//  Created by Go on 15/10/20.
+//  Copyright © 2015年 Go. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"sdsa'");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
